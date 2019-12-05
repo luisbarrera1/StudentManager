b57552fdfbc88702ec5269442ec0d885d009785d
luisbarrera1 <labarrera@mail.csuchico.edu>
2019-12-04 16:34:22 -0800

    first file student.h

---
+++ student.h
@@ -0,0 +1,21 @@
+//aithor luis Barrera
+#ifndef _STUDENT_H_
+#define _STUDENT_H_
+
+
+class student
+{
+        private:
+                string firstName;
+                string lastName;
+        Public:
+                void set firstName(string);
+                sting get firstName();
+
+};
+
+
+#endif
+
+
+
